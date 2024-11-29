cc4f7ada45ac1dadfb1ecc959e2d20ab232e9600
KARTIKESH SINGH <kartikeshking02@gmail.com>
2024-11-29 21:02:45 +0530

    Update threeone.js

--- week3/threeone.js
+++ week3/threeone.js
@@ -1,10 +1,17 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 app.use(express.json());
-app.post("/health",function(req,res){
-    const kidneys=req.headers.kidneys;
-    const kidneylength=kidneys.length;
-    
-    res.send("your kidney is "+kidneylength);
-})
+
+app.post("/health", function (req, res) {
+    const kidneys = req.body.kidneys;
+
+    if (!Array.isArray(kidneys)) {
+        return res.status(400).send("Invalid input: 'kidneys' should be an array.");
+    }
+
+    const kidneylength = kidneys.length;
+
+    res.send("You have " + kidneylength + " kidney(s).");
+});
+
 app.listen(3000);
